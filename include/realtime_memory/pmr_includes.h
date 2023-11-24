a8ca43b0e6326c931b5056db05fd632565a8030f
Tom Maisey <tommaisey@gmail.com>
2023-11-24 10:21:42 +0000

    Use non-experimental pmr on newer clang versions
    Clang 16 (alias 15 on Apple platforms) finally completed the
    memory_resource header, and using the experimental version
    now triggers a 'deprecated declaration' warning.

--- include/realtime_memory/pmr_includes.h
+++ include/realtime_memory/pmr_includes.h
@@ -3,10 +3,21 @@
 #include <cassert>
 #include <vector>
 
-#if defined(__clang__)
- #include <experimental/memory_resource>
- namespace std_pmr = std::experimental::pmr;
+#ifdef __clang__
+  #ifdef __apple_build_version__
+    #define USE_EXPERIMENTAL_PMR _LIBCPP_VERSION < 1500
+  #else
+    #define USE_EXPERIMENTAL_PMR _LIBCPP_VERSION < 1600
+  #endif
 #else
- #include <memory_resource>
- namespace std_pmr = std::pmr;
+  #define USE_EXPERIMENTAL_PMR 0
+#endif
+
+
+#if USE_EXPERIMENTAL_PMR
+  #include <experimental/memory_resource>
+  namespace std_pmr = std::experimental::pmr;
+#else
+  #include <memory_resource>
+  namespace std_pmr = std::pmr;
 #endif
