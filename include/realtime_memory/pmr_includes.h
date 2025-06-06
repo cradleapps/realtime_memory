f31788b8f7575f0c70d718094abd9eb878efee79
Tom Maisey <tommaisey@gmail.com>
2025-06-06 09:29:38 +0100

    Fix Apple SDK comparison (140000 should have been 101400)

--- include/realtime_memory/pmr_includes.h
+++ include/realtime_memory/pmr_includes.h
@@ -4,28 +4,34 @@
 #include <vector>
 
 #if defined (__apple_build_version__)
- #define USE_EXPERIMENTAL_PMR __apple_build_version__ < 15000000 || __MAC_OS_X_VERSION_MIN_REQUIRED < 140000
-#elif defined (__clang__)
- #define USE_EXPERIMENTAL_PMR _LIBCPP_VERSION < 1600
+  #if (__apple_build_version__ < 1500000)
+    #define USE_EXPERIMENTAL_PMR 1
+  #elif defined (__MAC_OS_X_VERSION_MIN_REQUIRED) && (__MAC_OS_X_VERSION_MIN_REQUIRED < 101400)
+    #define USE_EXPERIMENTAL_PMR 1
+  #else
+    #define USE_EXPERIMENTAL_PMR 0
+  #endif
+#elif defined (__clang__) && (_LIBCPP_VERSION < 1600)
+  #define USE_EXPERIMENTAL_PMR 1
 #else
- #define USE_EXPERIMENTAL_PMR 0
+  #define USE_EXPERIMENTAL_PMR 0
 #endif
 
 #if USE_EXPERIMENTAL_PMR
- #include <experimental/memory_resource>
- namespace std_pmr = std::experimental::pmr;
+  #include <experimental/memory_resource>
+  namespace std_pmr = std::experimental::pmr;
 #else
- #include <memory_resource>
- namespace std_pmr = std::pmr;
+  #include <memory_resource>
+  namespace std_pmr = std::pmr;
 #endif
 
 #if defined (__apple_build_version__) && USE_EXPERIMENTAL_PMR
- #define PMR_DIAGNOSTIC_PUSH \
-  _Pragma("clang diagnostic push") \
-  _Pragma("clang diagnostic ignored \"-Wdeprecated-declarations\"")
- #define PMR_DIAGNOSTIC_POP \
-  _Pragma("clang diagnostic pop")
+  #define PMR_DIAGNOSTIC_PUSH \
+     _Pragma("clang diagnostic push") \
+      _Pragma("clang diagnostic ignored \"-Wdeprecated-declarations\"")
+  #define PMR_DIAGNOSTIC_POP \
+     _Pragma("clang diagnostic pop")
 #else
- #define PMR_DIAGNOSTIC_PUSH
- #define PMR_DIAGNOSTIC_POP
+  #define PMR_DIAGNOSTIC_PUSH
+  #define PMR_DIAGNOSTIC_POP
 #endif
